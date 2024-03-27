149766bf47cf35236378c2630f2d8ef15254ea95
Ellis Osborn <ejosborn@asu.edu>
2024-03-26 23:28:09 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Task Tracker
+
+This is a CLI program that will the user can track tasks that they have to do throughout their day/week/month ect. It uses Flask for the backend for data processing and uses MongoDB to save data from the user in the database.
+
+## Author
+
+- **Ellis Osborn** [ejosborn](https://github.com/ejosborn)
---
+++ cli/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+
+    print("--------- Welcome to Task Manager ---------")
+    # commands
+    commands_dict = {
+        "help": "view all options to choose from",
+        "add": "add a task to the list",
+        "update": "update a task",
+        "delete": "delete a task from the list",
+    }

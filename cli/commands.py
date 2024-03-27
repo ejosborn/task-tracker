a6c841d132373238fc631e8713ea2d386643839f
Ellis Osborn <ejosborn@asu.edu>
2024-03-27 14:19:19 -0700

    created base commands file

---
+++ cli/commands.py
@@ -0,0 +1,29 @@
+def addTask():
+    print("Add a Task")
+
+
+def updateTask():
+    print("Update a task")
+
+
+def deleteTask():
+    print("Delete a task")
+
+
+def helpCommand():
+    # lists of commands
+    commands_dict = {
+        "help": "Displays Commands",
+        "add": "Add a task to the list",
+        "update": "Update a task",
+        "delete": "Delete a task from the list",
+        "exit": "Exit the program",
+    }
+    print("Here are the commands you can use:")
+
+    for key, value in commands_dict.items():
+        print(f"{key}\t -    {value}")
+
+
+def exitCommand():
+    exit()
